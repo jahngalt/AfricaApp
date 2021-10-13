67963b9af9b819e995f5bd14f0c0c09760c80228
Oleg Kudimov <ovkudimov@gmail.com>
2021-10-13 11:08:59 +0300

    Initial Commit

---
+++ AfricaApp/AfricaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAppApp.swift
+//  AfricaApp
+//
+//  Created by Oleg Kudimov on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
